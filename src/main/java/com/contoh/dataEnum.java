359f8060b95df7d9cbfa5e49ece1b4b9f7b0db8a
dimdims <imagaluh29@gmail.com>
2015-03-11 10:24:21 -0700

    class enum jk

---
+++ src/main/java/com/contoh/dataEnum.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.contoh;
+
+/**
+ *
+ * @author 1110100890
+ */
+public class dataEnum {
+    
+    public enum JenisKelamin {
+	LAKILAKI,PEREMPUAN
+    }
+    
+    public enum goldar {
+	A,B,O,AB
+    }
+    
+    public enum pnd {
+	TK,SD,SMP,SMA,D1,D2,D3,D4,S1,S2,S3
+    }
+    
+    public enum sttsNikah {
+	SINGLE,MENIKAH,JANDA,DUDA
+    }
+    
+    public enum keluarga {
+	AYAH,IBU,SUAMI,ISTRI,SAUDARA
+    } 
+}
